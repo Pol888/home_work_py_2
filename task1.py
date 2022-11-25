3a1ddd8b6dcb70877f92a054c0838eb57f970ff6
Павел <107019787+Pol888@users.noreply.github.com>
2022-11-25 10:56:49 +0500

    task1 is end, 2 to start

--- task1.py
+++ task1.py
@@ -1,9 +1,22 @@
-#x = float(input())
-x1 = 0.56 # 1.25 # 56
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
-
-while True:
+x1 = float(input()) + 100 # + 100, чтобы при * 10 не возникало погрешностей .. например: 0.56 * 10 = 5.6000000000000005
+sum_number = 0
+while x1 != 0:
     if x1 - int(x1) == 0:
-
+        sum_number += x1 % 10
+        x1 //= 10
     elif x1 - int(x1) != 0:
         x1 *= 10
+
+print(int(sum_number) - 1)
+
+# строковый вариант:
+sum_number2 = 0
+x2 = ''.join(input().split('.'))
+for i in x2:
+    sum_number2 += int(i)
+
+print(sum_number2)
+
+
